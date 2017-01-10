ed27dd163272ee604053384281fc06001b581f96
Dennis Smuda <dennissmuda@msn.com>
2017-01-10 14:30:32 +0100

    Medium Lightbox replicated

--- README.md
+++ README.md
@@ -8,3 +8,20 @@
 - ```git clone``` this repo
 - ```npm install```
 - ```npm start``` to build
+
+
+
+## Contents
+
+### ScrollFragment.js
+
+"Fragmentizes" a given container, by spreading its children across the page,
+once the triggering element reaches the top of the viewport.
+
+### ScrollFloat.js
+
+Allows to specify, the elements that should get a floating motion based on
+native scrolling behavior
+
+
+### Lightbox
--- src/js/Lightbox.js
+++ src/js/Lightbox.js
@@ -1,4 +1,8 @@
-
+/**
+ * Lightbox imitating Medium zoom effect
+ * @param {selector} element  [Selector for image elements]
+ * @param {type} _options [options - margins etc]
+ */
 function Lightbox(element, _options) {
   "use strict";
 
@@ -16,17 +20,17 @@ function Lightbox(element, _options) {
   const scrollBarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
   document.body.removeChild(scrollDiv);
 
-  console.log(document);
-  console.log(document.documentElement);
+  wrapImages();
 
 
+  // Update Dimensions and refresh cached variables
   function updateScreenSize() {
     let w = window,
         d = document,
         e = d.documentElement,
-        g = d.getElementsByTagName('body')[0],
-        x = w.innerWidth || e.clientWidth  || g.clientWidth,
-        y = w.innerWidth || e.clientHeight || g.clientHeight;
+        g = d.getElementsByTagName('body')[0];
+    let x = w.innerWidth  || e.clientWidth  || g.clientWidth;
+    let y = w.innerHeight || e.clientHeight || g.clientHeight;
         screenSize.x = x;
         screenSize.y = y;
   }
@@ -35,21 +39,126 @@ function Lightbox(element, _options) {
   updateScreenSize();
   window.addEventListener("resize", updateScreenSize);
 
-
+  // Zoom function
   function zoom() {
     if (!this.isZoomed) {
+      this.isZoomed = !this.isZoomed;
 
       zoomedImage = this;
-      console.log(zoomedImage);
+      if (!this.img) {
+        this.img = this.getElementsByTagName('img')[0];
+      }
+
+      // Image Dimensions
+      let imgH = this.img.getBoundingClientRect().height;
+      let imgW = this.img.getBoundingClientRect().width;
+      let imgL = this.img.getBoundingClientRect().left;
+      let imgT = this.img.getBoundingClientRect().top;
+      // Get real Dimensions
+      let realW = this.img.naturalWidth;
+      let realH = this.img.naturalHeight;
+
+      // Add class to image
+      if (this.img.classList) this.img.classList.add('zoomImg');
+      else this.img.className += ' ' + 'zoomImg';
+
+      // Create overlay
+      this.overlay = document.createElement('div');
+      this.overlay.id = 'overlay';
+      this.overlay.className = 'zoomOverlay';
+      this.overlay.style.cssText = `height: ${screenSize.y}px;
+                                    width: ${screenSize.x}px;`
+      this.overlay.style.cssText += 'top: -' + ((screenSize.y-imgH)/2) + 'px;';
+      this.overlay.style.cssText += 'left: -' + ((screenSize.x-imgW)/2) + 'px;';
+
+      // Create Image wrapper
+      this.wrapper = document.createElement('div');
+      this.wrapper.id = 'wrapper';
+      this.wrapper.className = 'zoomImg-wrap zoomImg-wrap--absolute';
+      this.wrapper.appendChild(this.img);
+
+      this.wrapper.appendChild(this.overlay);
+      this.appendChild(this.wrapper);
+
+      // Wrapper coords
+      let wrapX = ((screenSize.x-scrollBarWidth)/2)-imgL - (imgW/2);
+      let wrapY = imgT*(-1) + (screenSize.y-imgH)/2;
+
+
+      // Calculate scale
+      var scale = 1;
+      console.log(realH, imgH);
+      if (realH > imgH) {
+        if (imgH == imgW && screenSize.y > screenSize.x) {
+          // Square img and portrait aspect
+          scale = (screenSize.x-margin)/imgW;
+        } else if (imgH == imgW && screenSize.y < screenSize.x) {
+          // Square img and landscape aspect
+          scale = (screenSize.y-margin)/imgH;
+        } else if (imgH > imgW) {
+          // Portrait Image
+          scale = (screenSize.y-margin)/imgH;
+          if (scale*imgW > screenSize.x) {
+            // Zoomed image gets too big?
+            scale = (screenSize.x-margin)/imgW;
+          }
+        } else if (imgH < imgW) {
+          // Landscape Image
+          scale = (screenSize.x-margin)/imgW;
+          if (scale*imgH > screenSize.y) {
+            // Zoomed image gets too big
+            scale = (screenSize.y-margin)/imgH;
+          }
+        }
+      }
+
+      // Recalculate scale if zoomed image is bigger than the original
+      if (scale*imgW > realW) {
+        scale = realW/imgW;
+        console.log('Zoomed version is bigger than the original');
+      }
+
+
+      let that = this;
+      // Popout animation css
+      setTimeout(function() {
+        that.wrapper.style.cssText = `transform: translate(${wrapX}px, ${wrapY}px) translateZ(0px);`
+        that.wrapper.style.cssText+= `-webkit-transform: translate(${wrapX}px, ${wrapY}px) translateZ(0px);`
+
+        that.img.style.cssText= `transform: scale(${scale}); -webkit-transform: scale(${scale});`;
+        that.overlay.className = 'zoomOverlay show';
+      }, 0);
+
     } else {
+      // Put image back in place
+      this.isZoomed = !this.isZoomed;
+      // remove from zoomedImage
+      zoomedImage = null;
+
+      // Remove lightbox styles
+      this.img.style.cssText = "";
+      this.wrapper.style.cssText = "";
+      this.overlay.className = "zoomOverlay";
+
+      // Remove Element
+      let that = this;
+      setTimeout(function() {
+        that.children[0].appendChild(that.img);
+        that.removeChild(that.wrapper);
+
+        let className = 'zoomImg';
+        if (that.img.classList) {
+          that.img.classList.remove(className);
+        }
+      }, 300);
 
     }
   }
 
 
   // Apply effects on passed-in elements
-  const elements = document.querySelectorAll(element);
-  Array.prototype.forEach.call(elements, function(el, i) {
+  const ImageElements = document.querySelectorAll('figure');
+  Array.prototype.forEach.call(ImageElements, function(el, i) {
     el.addEventListener("click", zoom);
   });
 
@@ -62,5 +171,22 @@ function Lightbox(element, _options) {
 
   window.addEventListener("scroll", zoomOut);
 
+
+  function wrapImages() {
+    const ImageElements = document.querySelectorAll(element);
+    Array.prototype.forEach.call(ImageElements, function(el, i) {
+      // Wrapping figure
+      let figure = document.createElement("figure");
+      figure.classList = "one-third left zoom-effect";
+
+      let figureInner = document.createElement("div");
+      figureInner.classList = "aspectRatioPlaceholder";
+
+      el.parentNode.insertBefore(figureInner, el);
+      figureInner.parentNode.insertBefore(figure, el);
+      figureInner.appendChild(el);
+      figure.appendChild(figureInner);
+    });
+  }
 
 }
